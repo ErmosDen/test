346579f2e38c461dd246d0c9c60fdfe03febe5ce
ErmosDen <45843561+ErmosDen@users.noreply.github.com>
2018-12-14 11:59:27 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,8 +11,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Привет");
-            Console.WriteLine("Good Byes");
-            Console.WriteLine("Somthing");
+            Console.WriteLine("Good Byes163456534565346");
+            Console.WriteLine("Somthing43564565463546354563");
         }
     }
 }
