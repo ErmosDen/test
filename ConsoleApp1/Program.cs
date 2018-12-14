fd9ea16de6d5bc398bf6af4d435db9ffc26ee097
denis <denis@DENIS-PC>
2018-12-14 11:48:17 +0300

    изменение

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello Worlds");
             Console.WriteLine("Good Byes");
+            Console.WriteLine("Somthing");
         }
     }
 }
